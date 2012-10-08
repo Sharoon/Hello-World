65d502a3f1ed569e8582c2aff597506af8aeed49
Sharoon Arif <sa289@uni.brighton.ac.uk>
2012-10-08 11:03:21 +0100

    git add Lab1/src/Lab1.cpp

---
+++ Lab1/src/Lab1.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Lab1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+
+	int time;
+	time = time;
+
+		cout << time;
+
+	return 0;
+}
