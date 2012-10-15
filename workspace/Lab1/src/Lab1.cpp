97eae4f691248ac9a04352ee8e72b1d50fc14e9d
Sharoon Arif <sa289@uni.brighton.ac.uk>
2012-10-15 10:41:39 +0100

    first commit

---
+++ workspace/Lab1/src/Lab1.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Lab1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+
+	int i;
+	int box_size;
+	cout << "Please Enter Box Size : ";
+	cin >> box_size;
+
+		for (i=0;i<box_size;i++)
+		{
+			cout<<"--";
+		}
+
+	return 0;
+}
